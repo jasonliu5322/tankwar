4eb5d9af7626754f7c5dee81523bd8dd62f28a95
Hongliang Liu <jasonliu5322@gmail.com>
2020-07-25 13:30:06 -0700

    update tank's 8 directions control

---
+++ src/main/java/tankwar/Direction.java
@@ -0,0 +1,12 @@
+package tankwar;
+
+public enum Direction {
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT,
+    UPLEFT,
+    UPRIGHT,
+    DOWNLEFT,
+    DOWNRIGHT
+}
--- src/main/java/tankwar/GameClient.java
+++ src/main/java/tankwar/GameClient.java
@@ -2,28 +2,53 @@ package tankwar;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class GameClient extends JComponent {
 
+    private Tank playerTank;
+
     public GameClient(){
+        this.playerTank = new Tank(400, 100, Direction.DOWN);
         this.setPreferredSize(new Dimension(800, 600));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(),400,100,null);
+        playerTank.draw(g);
     }
 
     public static void main(String[] args) {
         JFrame frame =  new JFrame();
         frame.setTitle("New Tankwar");
         frame.setIconImage(new ImageIcon("assets/images/icon.png").getImage());
-        GameClient client = new GameClient();
+        final GameClient client = new GameClient();
         frame.add(client);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();//make the frame size normal
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                client.playerTank.keyPressed(e);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                client.playerTank.keyReleased(e);
+
+            }
+        });
         frame.setLocationRelativeTo(null);//put the frame in the middle of screen
         frame.setVisible(true);
+
+        while(true){
+            client.repaint();
+            try{
+                Thread.sleep(50);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
