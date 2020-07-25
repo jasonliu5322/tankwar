2c04b0f4f04c87f6a3e20354b54bb0f3f8df1303
Hongliang Liu <jasonliu5322@gmail.com>
2020-07-25 16:21:42 -0700

    adding enemy tanks

--- src/main/java/tankwar/GameClient.java
+++ src/main/java/tankwar/GameClient.java
@@ -1,22 +1,37 @@
 package tankwar;
 
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameClient extends JComponent {
 
     private Tank playerTank;
+    private List<Tank> enemyTanks;
+
 
     public GameClient(){
         this.playerTank = new Tank(400, 100, Direction.DOWN);
+        this.enemyTanks = new ArrayList<>(12);
+        for(int i = 0; i < 3; i ++){
+            for (int j = 0; j < 4 ; j++) {
+                this.enemyTanks.add(new Tank(200 + 50 * j, 400 + 40 * i, true, Direction.UP ));
+            }
+        }
+
         this.setPreferredSize(new Dimension(800, 600));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         playerTank.draw(g);
+        for(Tank tank: enemyTanks){
+            tank.draw(g);
+        }
     }
 
     public static void main(String[] args) {
